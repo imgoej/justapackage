b90c247af08f120ea897dbcbba1fa7d2f42b9967
Frederik Harly Baumgarten <76141501+imgoej@users.noreply.github.com>
2022-08-30 15:19:59 +0200

    add: my_index_adjuster

---
+++ R/my_index_adjuster.R
@@ -0,0 +1,17 @@
+# my_index_adjuster ####
+
+#' my_index_adjuster
+#' @export
+my_index_adjuster <- function(index = NULL, add = NULL, remove = NULL) {
+  if(!is.null(add)){
+    for (i in 1:length(EX)) {
+      EX[[i]][add] <- TRUE
+    }
+  }
+  if(!is.null(remove)){
+    for (i in 1:length(EX)) {
+      EX[[i]][remove] <- FALSE
+    }
+  }
+  EX
+}
