c1337a3397f6c05a9869ec306b4eb963d1bd586e
Frederik Harly Baumgarten <fhb@outlook.dk>
2023-02-23 09:27:44 +0100

    add function: my_nll_desaver

---
+++ R/my_nll_desaver.R
@@ -0,0 +1,21 @@
+# my_nll_desaver ####
+
+#' my_nll_desaver
+#'
+#' @export
+my_nll_desaver <- function(x, bin.file) {
+  # create matrix with rows of 6 WITHOUT duplicated values if length is not a match
+  X <- matrix(x[1:(6 * ceiling(length(x)/6))], nrow = 6)
+
+  # if length of x is not a multiple of 6, make a blank cell
+  X[is.na(X)] <- ""
+
+  # convert matrix to a data.frame
+  X <- as.data.frame(X)
+
+  # set binx-file name as column name
+  colnames(X) <- rep(binx.file, ncol(X))
+
+  # copy to clipboard
+  write.table(X, "clipboard", sep = "\t", col.names = T, row.names = F)
+}
